7877a3c6c949366031217da5e4cbd81d89353075
heahan <78015933+heahan@users.noreply.github.com>
2021-02-05 19:41:40 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,19 +3,26 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-        TG_BOT_TOKEN= "1671340816:AAG2RUyHa795nWXQDeNhjXGpnzf678Zs7IE" #imp
-            APP_ID = 2349847 #imp
-                API_HASH = "ba712182c38536fc7ce235f972834eb3" #imp
-                    AUTH_CHANNEL = [-1001463269462, 1473490396] #imp replace your_id with your id from telegram or delete
-                        INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
-                            GLEECH_COMMAND = "gleech@DarkHacker_Leech_Bot"
-                                YTDL_COMMAND = 'ytdl@DarkHacker_Leech_Bot'
-                                    TELEGRAM_LEECH_COMMAND_G = "tleech@DarkHacker_Leech_Bot"
-                                        CLONE_COMMAND_G = "gclone@DarkHacker_Leech_Bot"
-                                            PYTDL_COMMAND_G = "pytdl@DarkHacker_Leech_Bot"
-                                                DESTINATION_FOLDER = "TorrentLeech-Gdrive"
-                                                    LEECH_COMMAND = "leech@DarkHacker_Leech_Bot"
-                                                        CANCLE_COMMAND_G = "cancel@DarkHacker_Leech_Bot"
-                                                            RCLONE_CONFIG = " " " " "
-                                                                #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
-                                                                
+            TG_BOT_TOKEN= "1671340816:AAG2RUyHa795nWXQDeNhjXGpnzf678Zs7IE" #imp
+                    APP_ID = 2349847 #imp
+                            API_HASH = "ba712182c38536fc7ce235f972834eb3" #imp
+                                    AUTH_CHANNEL = [-1001463269462, 1473490396] #imp replace your_id with your id from telegram or delete
+                                            GLEECH_COMMAND = "gleech@Dark_Hackers_leecher_bot"
+                                                    YTDL_COMMAND = 'ytdl@Dark_Hackers_leecher_bot'
+                                                            TELEGRAM_LEECH_COMMAND_G = "tleech@Dark_Hackers_leecher_bot"
+                                                                    CLONE_COMMAND_G = "gclone@Dark_Hackers_leecher_bot"
+                                                                            PYTDL_COMMAND_G = "pytdl@Dark_Hackers_leecher_bot"
+                                                                                    DESTINATION_FOLDER = "Download"
+                                                                                            LEECH_COMMAND = "leech@torlechlkbot"
+                                                                                                    INDEX_LINK = "https://allinone.darkkali614.workers.dev/0:"
+                                                                                                            RCLONE_CONFIG = """
+                                                                                                            [kmac]
+                                                                                                            type = drive
+                                                                                                            client_id = 24644445636-le8qtpevii1jf672k7seeg0u1ru5vp3j.apps.googleusercontent.com
+                                                                                                            client_secret = 7129Wg4PYqfTgOqJZDVWyS2I
+                                                                                                            scope = drive
+                                                                                                            root_folder_id =
+                                                                                                            token = {"access_token":"ya29.a0AfH6SMBGancUBWLmfF2MODJpNhyRg5hHM0JmzGaVf5kHlN_HGfisP-EYvQoyliKysbvKBvonmMeWy981Fb5C_cJXp7w0_L22OYrveuQg_rLyGWXzr1VQe05oHRee8UnmVyBijEvNJRkSTGYltOPeS_8EqZPZfOBQCk6obbwnbzA","token_type":"Bearer","refresh_token":"1//0g-ByVm7cD9_eCgYIARAAGBASNwF-L9IrnQVGb5UdfeMdrH1JjL93Z89sQwBtxlUKSk1eSuKQXcSxPfNRTegxif6jylpUyuh0dTg","expiry":"2021-01-22T09:38:19.9912246-08:00"}
+                                                                                                            team_drive = 0ALyFD2RofrpdUk9PVA 
+                                                                                                            """
+                                                                                                                
