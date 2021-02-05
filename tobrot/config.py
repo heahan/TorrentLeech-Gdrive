ff8c56480e8523aafdaa9c96197cb68777ed9a56
heahan <78015933+heahan@users.noreply.github.com>
2021-02-05 19:01:17 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,19 +3,19 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-    TG_BOT_TOKEN= "1671340816:AAG2RUyHa795nWXQDeNhjXGpnzf678Zs7IE" #imp
-        APP_ID = 2349847 #imp
-            API_HASH = "ba712182c38536fc7ce235f972834eb3" #imp
-                AUTH_CHANNEL = [-1001463269462, 1473490396] #imp replace your_id with your id from telegram or delete
-                    INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
-                        GLEECH_COMMAND = "gleech@DarkHacker_Leech_Bot"
-                            YTDL_COMMAND = 'ytdl@DarkHacker_Leech_Bot'
-                                TELEGRAM_LEECH_COMMAND_G = "tleech@DarkHacker_Leech_Bot"
-                                    CLONE_COMMAND_G = "gclone@DarkHacker_Leech_Bot"
-                                        PYTDL_COMMAND_G = "pytdl@DarkHacker_Leech_Bot"
-                                            DESTINATION_FOLDER = "TorrentLeech-Gdrive"
-                                                LEECH_COMMAND = "leech@DarkHacker_Leech_Bot"
-                                                    CANCLE_COMMAND_G = "cancel@DarkHacker_Leech_Bot"
-                                                        RCLONE_CONFIG = " " " " "
-                                                            #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
-                                                            
+        TG_BOT_TOKEN= "1671340816:AAG2RUyHa795nWXQDeNhjXGpnzf678Zs7IE" #imp
+            APP_ID = 2349847 #imp
+                API_HASH = "ba712182c38536fc7ce235f972834eb3" #imp
+                    AUTH_CHANNEL = [-1001463269462, 1473490396] #imp replace your_id with your id from telegram or delete
+                        INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
+                            GLEECH_COMMAND = "gleech@DarkHacker_Leech_Bot"
+                                YTDL_COMMAND = 'ytdl@DarkHacker_Leech_Bot'
+                                    TELEGRAM_LEECH_COMMAND_G = "tleech@DarkHacker_Leech_Bot"
+                                        CLONE_COMMAND_G = "gclone@DarkHacker_Leech_Bot"
+                                            PYTDL_COMMAND_G = "pytdl@DarkHacker_Leech_Bot"
+                                                DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+                                                    LEECH_COMMAND = "leech@DarkHacker_Leech_Bot"
+                                                        CANCLE_COMMAND_G = "cancel@DarkHacker_Leech_Bot"
+                                                            RCLONE_CONFIG = " " " " "
+                                                                #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
+                                                                
